5405b0b9f712c6e4f98c975bf854274b8a43cf72
mknizewski <mateusz.knizewski@gmail.com>
2015-10-16 23:29:20 +0200

    Dodanie podstawowej funkcjonalności newslettera

--- SportStore.Web/Controllers/HomeController.cs
+++ SportStore.Web/Controllers/HomeController.cs
@@ -3,15 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SportStore.Web.Models.Home;
 
 namespace SportStore.Web.Controllers
 {
+    /// <summary>
+    /// Autor:  Mateusz Kniżewski
+    /// Opis:   Klasa kontrolera głownej strony
+    /// Data:   16.10.15
+    /// </summary>
     public class HomeController : Controller
     {
-        // GET: Index
+        #region Metody Kontrolera
+
+        /// <summary>
+        /// Metoda defaultowa
+        /// </summary>
+        /// <returns>Widok Index</returns>
         public ActionResult Index()
         {
             return View();
         }
+
+        /// <summary>
+        /// Metoda wyswietlajaca widok zapisu do newslettera
+        /// </summary>
+        /// <returns>Widok SaveNewsletter</returns>
+        [HttpGet]
+        public ViewResult SaveNewsletter()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Metoda zapisująca model newsletter
+        /// </summary>
+        /// <param name="newsletter">obiekt typu newsletter</param>
+        /// <returns>Widok NewsletterThanks</returns>
+        [HttpPost]
+        public ViewResult SaveNewsletter(Newsletter newsletter)
+        {
+            return View("NewsletterThanks", newsletter);
+        }
+
+        #endregion
     }
 }
---
+++ SportStore.Web/Models/Home/Newsletter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SportStore.Web.Models.Home
+{
+    /// <summary>
+    /// Autor:  Mateusz Kniżewski
+    /// Opis:   Klasa modelowa do obsługi newslettera
+    /// Data:   16.10.15
+    /// </summary>
+    public class Newsletter
+    {
+        [Display(Name = "E-mail:")]
+        [Required(ErrorMessage = "Pole E-mail jest wymagane!")]
+        public string Email { get; set; }
+
+        [Display(Name = "Jaki dział chcesz subskrybować:")]
+        [Required(ErrorMessage = "Proszę określić dział")]
+        public int TypeOfNews { get; set; }
+
+        [Display(Name =  "Częstotliwość wiadomości:")]
+        [Required(ErrorMessage = "Proszę określić częstotliwość")]
+        public int Frequency { get; set; }
+    }
+}
